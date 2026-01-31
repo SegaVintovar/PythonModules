ad9864ebcc1ca2a3944d519eb6a743582b64eb90
vsudak <vsudak@student.codam.nl>
2026-01-31 19:49:20 +0100

    ex4

---
+++ mod1/ex4/ft_garden_security.py
@@ -0,0 +1,16 @@
+class SecurePlant:
+	# name = None
+	# height = None
+	# age = None
+	def __init__(self, name, height, age):
+		self.name = name
+		self.height = height
+		self.age = age
+	def set_height(self, size):
+		if size >=0:
+			self.height = size
+		else
+			print('')
+	def set_age(self, days):
+		if days >= 0:
+			self.age = days
