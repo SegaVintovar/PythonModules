62614ec034705e9767c157636ebdc61bf52fcd25
vsudak <vsudak@student.codam.nl>
2026-03-02 10:23:32 +0100

    mod4 done

--- module04/ex4/ft_crisis_response.py
+++ module04/ex4/ft_crisis_response.py
@@ -1,33 +1,47 @@
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+
 def main() -> None:
     print("=== CYBER ARCHIVES - CRISIS RESPONSE SYSTEM ===\n")
     fake_open = "lost_archive.txt"
     try:
+        print("CRISIS ALERT: Attempting access to ", fake_open)
         with open(fake_open) as fake_file:
             print(fake_file.read())
     except FileNotFoundError:
-            print("RESPONSE: Archive not found in storage matrix")
-    finally:
+        print("RESPONSE: Archive not found in storage matrix", file=sys.stderr)
+    else:
         fake_file.close()
+    finally:
         print("STATUS: Crisis handled, system stable")
     print()
-    forbidden_file = "classified_vault.txt"
+    # forbidden_file = "classified_data.txt"
+    forbidden_file = BASE_DIR / "classified_data.txt"
     try:
-        with open(forbidden_file) as no_permission_file:
+        print("CRISIS ALERT: Attempting access to 'classified_vault.txt'...")
+        with open(forbidden_file, "r") as no_permission_file:
             print(no_permission_file.read())
     except PermissionError:
-            print("RESPONSE: Security protocols deny access")
+        print("RESPONSE: Security protocols deny access", file=sys.stderr)
+    else:
+        no_permission_file.close()
     finally:
-        forbidden_file.close()
         print("STATUS: Crisis handled, security maintained")
     print()
-    std_file = "standard_archive.txt"
+    # std_file = "/mnt/c/Users/Laptop/study/python/Python_modules/module04/ex4/standard_archive.txt"
+    std_file = BASE_DIR / "standard_archive.txt"
     try:
+        print("CRISIS ALERT: Attempting access to 'classified_vault.txt'...")
         with open(std_file) as file:
             print("SUCCESS: Archive recovered -", file.read())
     except Exception as e:
-            print(str(e))
-    finally:
+        print("FAIL: ", str(e), file=sys.stderr)
+    else:
         file.close()
+    finally:
         print("STATUS: Normal operations resumed")
     
 if __name__ == "__main__":
