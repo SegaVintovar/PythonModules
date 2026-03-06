ff3e1bd7226d3054c4bee7586a1449ab23799bc0
vsudak <vsudak@student.codam.nl>
2026-03-06 10:43:15 +0100

    ex2 started

--- module05/ex2/nexus_pipeline.py
+++ module05/ex2/nexus_pipeline.py
@@ -3,6 +3,67 @@ from typing import Any, List, Optional, Union, Dict
 
 
 class ProcessingPipeline(ABC):
-    @abstractmethod
+    def __init__(self, data: Any):
+        self.data = data
+        print("Creating Data Processing Pipeline...")
+        self.stage1 = InputStage()
+        self.stage2 = TransformStage()
+        self.stage3 = OutputStage()
+
+    # @abstractmethod
+    def process(self, data: Any) -> Any:
+        input = self.stage1.process(data)
+        transformed = self.stage2.process(input)
+        output = self.stage3.process(transformed)
+        return output
+
+
+class InputStage(ProcessingPipeline):
+    def __init__(self):
+        print("Stage 1: Input validation and parsing")
+
     def process(self, data: Any) -> Any:
         ...
+
+
+class TransformStage(ProcessingPipeline):
+    def __init__(self):
+        print("Stage 2: Data transformation and enrichment")
+
+    def process(self, data: Any) -> Any:
+        ...
+
+
+class OutputStage(ProcessingPipeline):
+    def __init__(self):
+        print("Stage 3: Output formatting and delivery")
+
+    def process(self, data: Any) -> Any:
+        ...
+
+
+class NexusManager():
+    def __init__(self):
+        print("Initializing Nexus Manager...")
+        print("Pipeline capacity: 1000 streams/second")
+
+class JSONAdapter(ProcessingPipeline):
+    def process(self, data: Any) -> Any:
+        ...
+    
+class CSVAdapter(ProcessingPipeline):
+    def process(self, data: Any) -> Any:
+        ...
+    
+class StreamAdapter(ProcessingPipeline):
+    def process(self, data: Any) -> Any:
+        ...
+
+def main():
+    pipe = ProcessingPipeline()
+    data1 = {"sensor": "temp", "value": 23.5, "unit": "C"}
+    data2 = "user,action,timestamp"
+
+
+if __name__ == "__main__":
+    main()
